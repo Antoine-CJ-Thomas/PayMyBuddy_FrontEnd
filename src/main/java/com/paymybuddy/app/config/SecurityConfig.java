0387481e4aa0331c41c3834eb32abf50ab9d2c10
Antoine-CJ-Thomas <antoine.cj.thomas@gmail.com>
2021-03-26 17:52:21 +0100

    Modification of SecurityConfig BCryptPasswordEncoder strenght

--- src/main/java/com/paymybuddy/app/config/SecurityConfig.java
+++ src/main/java/com/paymybuddy/app/config/SecurityConfig.java
@@ -63,6 +63,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(12);
     }
 }
