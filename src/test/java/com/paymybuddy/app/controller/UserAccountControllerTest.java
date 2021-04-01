50ed1cdbf4149d8fbeb56a06d002411749f4a691
Antoine-CJ-Thomas <antoine.cj.thomas@gmail.com>
2021-04-01 16:04:24 +0200

    Renamed a unit test package

--- src/test/java/com/paymybuddy/app/controller/UserAccountControllerTest.java
+++ src/test/java/com/paymybuddy/app/controller/UserAccountControllerTest.java
@@ -1,4 +1,4 @@
-package com.paymybuddy.app.unit.controller;
+package com.paymybuddy.app.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
